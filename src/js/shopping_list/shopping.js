ce317255a6d3855e3a74a18afca778cab5e2edae
ribacot <remontdiskov1979@gmail.com>
2023-07-08 09:53:39 +0300

    start_js_html

---
+++ src/js/shopping_list/shopping.js
@@ -0,0 +1,65 @@
+import fetch from './serviceBook';
+
+const listBooksEl = document.querySelector('.js-list-books');
+console.log(listBooksEl);
+async function addLocalStoredge(data) {
+  localStorage.setItem('shopingBooks', JSON.stringify(await data()));
+}
+addLocalStoredge(fetch);
+
+const books = JSON.parse(localStorage.getItem('shopingBooks'));
+console.log('books', books);
+
+function marcupListBooks(arr) {
+  return arr
+    .map(
+      ({
+        title,
+        book_image,
+        author,
+        description,
+        list_name,
+        buy_links,
+      }) => `<li>
+            <div><img src="${book_image}" alt="${title}" /></div>
+            <div>
+              <div>
+                <h2>${title}</h2>
+                <h3>${list_name}</h3>
+                <p>${description}</p>
+              </div>
+              <div>
+                <span>${author}</span>
+                <ul>
+                <li> 
+                 <a href="${
+                   buy_links.filter(({ name }) => {
+                     return name === 'Amazon';
+                   })[0].url
+                 }))}">Amazon</a>
+</li>
+<li> 
+  <a href="${
+    buy_links.filter(({ name }) => {
+      return name === 'Apple Books';
+    })[0].url
+  }">Apple Books</a>
+</li>
+<li> 
+<a href="${
+        buy_links.filter(({ name }) => {
+          return name === 'Bookshop';
+        })[0].url
+      }">Bookshop</a>
+</li></ul>
+                </div>
+              </div>
+            </div>
+            <button></button>
+          </li>
+`
+    )
+    .join('');
+}
+
+listBooksEl.innerHTML = marcupListBooks(books);
