ad78cf4d1701c4adaad2af50cda26f4de8dd8854
ribacot <remontdiskov1979@gmail.com>
2023-07-09 16:35:52 +0300

    fit_button_del

--- src/js/shopping_list/shopping.js
+++ src/js/shopping_list/shopping.js
@@ -2,21 +2,51 @@ import fetch from './serviceBook';
 import { marcupListBooks } from './marcupListBooks';
 
 const listBooksEl = document.querySelector('.js-list-books');
-console.log(listBooksEl);
+
 async function addLocalStoredge(data) {
   localStorage.setItem('shopingBooks', JSON.stringify(await data()));
 }
+
 addLocalStoredge(fetch);
 
 try {
   const books = JSON.parse(localStorage.getItem('shopingBooks'));
   // console.log('books', books);
   // const books = [];
-  if (!books) {
+  if (!books||!books.length) {
     // console.log("fuck!!!", books.length);
-    listBooksEl.innerHTML = `<p>This page is empty, add some books and proceed to order.</p><img src="./images/shopping_List/IMG_9606 1.png" alt="">
-`}else{  listBooksEl.innerHTML = marcupListBooks(books);
+ isEmpty()
+  } else {
+    listBooksEl.innerHTML = marcupListBooks(books);
+    // const btnDelEl = document.querySelectorAll('.js-trash');
+    // console.log(books)
+    listBooksEl.addEventListener('click', onRemuveCard);
+
+    function onRemuveCard(e) {
+
+      if (!e.target.classList.contains('js-trash')) {
+        return;
+      }
+      console.log(books);
+
+      console.log('targetId', e.target.dataset.id);
+      const indexDel = books.findIndex(
+        ({ _id }) => _id === e.target.dataset.id
+      );
+      console.log(indexDel);
+      if (indexDel !== -1) {
+        books.splice(indexDel, 1);
+        localStorage.setItem('shopingBooks', JSON.stringify(books));
+        if(!books.length){return isEmpty()}
+        listBooksEl.innerHTML = marcupListBooks(books);
+      }
+    }
+  }
+} catch (err) {
+  console.log(err);
 }
 
-} catch (err) { console.log(err) }
-
+function isEmpty() {
+     listBooksEl.innerHTML = `<p>This page is empty, add some books and proceed to order.</p><img src="./images/shopping_List/IMG_9606 1.png" alt="">
+`;
+}
