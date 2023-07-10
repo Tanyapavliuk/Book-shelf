a550c5603f7c727b3c187379a99b2e5ca834e732
BorysShabazov <122737738+BorysShabazov@users.noreply.github.com>
2023-07-10 23:55:39 +0200

    and-js-header-and-login

--- src/js/header.js
+++ src/js/header.js
@@ -1,8 +1,28 @@
 const loginBtnEl = document.querySelector('.sing-up-btn');
+const loginBtnMobEl = document.querySelector('.sing-up-btn-mob');
 const backdropLoginEl = document.querySelector('.backdrop-login');
 const modalLoginEl = document.querySelector('.login');
 const closeModalLoginEl = document.querySelector('.modal-login-close');
-const loginBtnMobEl = document.querySelector('.sing-up-btn-mob');
+const formEl = document.querySelector('.login-form');
+const singUpEl = document.querySelector('.sing-up-js');
+const singInEl = document.querySelector('.sing-in-js');
+const submitBtnEl = document.querySelector('.sub-btn-js');
+const nameEl = document.querySelector('.name-js');
+const userBtnEl = document.querySelector('.user-btn');
+const userDescEl = document.querySelector('.user');
+const userMobEl = document.querySelector('.user-mob');
+const userNameEl = document.querySelector('.name-user');
+const userNameMobEl = document.querySelector('.name-user-mob');
+const logoutDescEl = document.querySelector('.log-out-decs');
+const logoutMobEl = document.querySelector('.log-out-mob');
+const humbMenuEl = document.querySelector('.hamburger-menu');
+const mobMenuEl = document.querySelector('.mobile-menu');
+const mobCloseEl = document.querySelector('.mobile-close');
+const bodyEl = document.querySelector('body');
+const spanSingUpEl = document.querySelector('.span-sing-up');
+const spanSingInEl = document.querySelector('.span-sing-in');
+
+// відкриття модалки логіну
 
 loginBtnEl.addEventListener('click', handlerOpenLoginModal);
 loginBtnMobEl.addEventListener('click', handlerOpenLoginModal);
@@ -18,9 +38,17 @@ function handlerCloseLoginModal() {
   modalLoginEl.classList.remove('active');
 }
 
-const humbMenuEl = document.querySelector('.hamburger-menu');
-const mobMenuEl = document.querySelector('.mobile-menu');
-const mobCloseEl = document.querySelector('.mobile-close');
+// виключення скролу
+
+function scrollOff() {
+  if (humbMenuEl.classList.contains('visually-hidden')) {
+    bodyEl.style.overflow = 'hidden';
+  } else {
+    bodyEl.style.overflow = 'visible';
+  }
+}
+
+// відкриття мобільного меню
 
 humbMenuEl.addEventListener('click', handlerOpenMobMenu);
 mobCloseEl.addEventListener('click', handlerCloseMobMenu);
@@ -29,12 +57,16 @@ function handlerOpenMobMenu() {
   humbMenuEl.classList.add('visually-hidden');
   mobMenuEl.style.left = '0';
   mobCloseEl.classList.remove('visually-hidden');
+  scrollOff();
 }
 function handlerCloseMobMenu() {
   humbMenuEl.classList.remove('visually-hidden');
   mobMenuEl.style.left = '100%';
   mobCloseEl.classList.add('visually-hidden');
+  scrollOff();
 }
+
+// зміна теми
 
 const switchThemeEl = document.querySelector('.switch');
 const checkboxEl = document.querySelector('.checkbox-theme');
@@ -46,13 +78,9 @@ function switchPosition() {
 }
 switchPosition();
 
-const formEl = document.querySelector('.login-form');
-const singUpEl = document.querySelector('.sing-up-js');
-const singInEl = document.querySelector('.sing-in-js');
-const submitBtnEl = document.querySelector('.sub-btn-js');
+// опрацювання модалки логіну, реестрація і логінізація
 
 submitBtnEl.textContent = 'Sing up';
-let usersId = 0;
 
 singUpEl.addEventListener('click', handleSingUp);
 singInEl.addEventListener('click', handlesingIn);
@@ -60,19 +88,27 @@ singInEl.addEventListener('click', handlesingIn);
 function handleSingUp() {
   singInEl.classList.add('disabled-btn');
   singUpEl.classList.remove('disabled-btn');
+  spanSingInEl.classList.remove('active-btn');
+  spanSingUpEl.classList.add('active-btn');
   submitBtnEl.textContent = 'Sing up';
+  nameEl.classList.remove('visually-hidden');
 }
 
 function handlesingIn() {
   singUpEl.classList.add('disabled-btn');
   singInEl.classList.remove('disabled-btn');
-
+  spanSingInEl.classList.add('active-btn');
+  spanSingUpEl.classList.remove('active-btn');
+  nameEl.classList.add('visually-hidden');
   submitBtnEl.textContent = 'Sing in';
 }
 
-formEl.addEventListener('submit', handlerForm);
+formEl.addEventListener('submit', handlerFormReg);
+formEl.addEventListener('submit', handlerFormLogin);
 
-function handlerForm(evt) {
+// реестрація
+
+function handlerFormReg(evt) {
   evt.preventDefault();
 
   if (
@@ -81,21 +117,114 @@ function handlerForm(evt) {
     evt.target.useremail.value &&
     evt.target.userpass.value
   ) {
-    const user = {
-      name: evt.target.username.value,
-      email: evt.target.useremail.value,
-      password: evt.target.userpass.value,
-    };
-
-    localStorage.setItem(`user${usersId}`, JSON.stringify(user));
-    usersId += 1;
+    if (!localStorage.getItem('users')) {
+      const users = {
+        users: [
+          {
+            name: evt.target.username.value,
+            email: evt.target.useremail.value,
+            password: evt.target.userpass.value,
+          },
+        ],
+      };
+      localStorage.setItem(`users`, JSON.stringify(users));
+    } else {
+      const addUsers = JSON.parse(localStorage.getItem('users'));
+      addUsers.users.push({
+        name: evt.target.username.value,
+        email: evt.target.useremail.value,
+        password: evt.target.userpass.value,
+      });
+      localStorage.removeItem('users');
+      localStorage.setItem(`users`, JSON.stringify(addUsers));
+    }
 
     evt.target.username.value = '';
     evt.target.useremail.value = '';
     evt.target.userpass.value = '';
 
     handlesingIn();
+  } else if (submitBtnEl.textContent !== 'Sing up') {
+    return;
   } else {
     console.log('Заповніть усі поля');
+  }
+}
+
+// логінізація
+
+function handlerFormLogin(evt) {
+  evt.preventDefault();
+
+  if (
+    submitBtnEl.textContent === 'Sing in' &&
+    evt.target.useremail.value &&
+    evt.target.userpass.value
+  ) {
+    const { users } = JSON.parse(localStorage.getItem('users'));
+
+    const user = users.find(
+      user =>
+        user.email === evt.target.useremail.value &&
+        user.password === evt.target.userpass.value
+    );
+
+    localStorage.setItem('userLogin', true);
+    localStorage.setItem('userInSite', JSON.stringify(user));
+
+    evt.target.useremail.value = '';
+    evt.target.userpass.value = '';
+
+    handlerCloseLoginModal();
+
+    userNameMobEl.textContent = user.name;
+    userNameEl.textContent = user.name;
+    loginBtnMobEl.classList.add('visually-hidden');
+    loginBtnEl.classList.add('visually-hidden');
+    userDescEl.classList.remove('visually-hidden');
+    userMobEl.classList.remove('visually-hidden');
+    logoutMobEl.classList.remove('visually-hidden');
+  }
+}
+
+// перевірка при перезавантаженні сторінки
+
+function checkUser() {
+  if (localStorage.getItem('userLogin')) {
+    const user = JSON.parse(localStorage.getItem('userInSite'));
+    userNameMobEl.textContent = user.name;
+    userNameEl.textContent = user.name;
+    loginBtnMobEl.classList.add('visually-hidden');
+    loginBtnEl.classList.add('visually-hidden');
+    userDescEl.classList.remove('visually-hidden');
+    userMobEl.classList.remove('visually-hidden');
+    logoutMobEl.classList.remove('visually-hidden');
+  }
+}
+checkUser();
+
+// вихід
+
+logoutDescEl.addEventListener('click', handlerLogout);
+logoutMobEl.addEventListener('click', handlerLogout);
+userBtnEl.addEventListener('click', handlerOpenLogout);
+
+function handlerLogout() {
+  logoutDescEl.classList.add('visually-hidden');
+  logoutMobEl.classList.add('visually-hidden');
+  userDescEl.classList.add('visually-hidden');
+  userMobEl.classList.add('visually-hidden');
+  loginBtnMobEl.classList.remove('visually-hidden');
+  loginBtnEl.classList.remove('visually-hidden');
+  localStorage.removeItem('userLogin');
+  localStorage.removeItem('userInSite');
+  handlerCloseMobMenu();
+}
+
+function handlerOpenLogout() {
+  if (logoutDescEl.classList.contains('visually-hidden')) {
+    logoutDescEl.classList.remove('visually-hidden');
+  } else {
+    logoutDescEl.classList.add('visually-hidden');
   }
 }
