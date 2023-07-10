bdbbbccd3148f2e76d59628812bd683738a4cef7
Tanyapavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2023-07-10 20:21:34 +0300

    refresh js in shopping list

--- src/js/shopping_list/shopping_main.js
+++ src/js/shopping_list/shopping_main.js
@@ -1,10 +1,7 @@
 import './shopping';
 import '../dark-theme';
 // import '../loader';
-import '../modal';
 // import '../scrollUp';
 import '../support';
 import '../modalLogin';
-import '../hero';
-import '../categories';
 import '../header'
