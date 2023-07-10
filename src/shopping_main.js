957630b46d2bdb46d12f8589c4c1af949a9ba197
ribacot <remontdiskov1979@gmail.com>
2023-07-11 00:08:41 +0300

    fit_loder_scroll_fix_mainJS

---
+++ src/shopping_main.js
@@ -0,0 +1,10 @@
+import './js/header';
+import './js/dark-theme';
+// import './js/categories';
+import './js/support';
+// import './js/hero';
+import "./js/shopping_list/shopping";
+// import './js/modal';
+import './js/modalLogin';
+import './js/loader';
+import './js/scrollUp';
