0a48f2dfb6fd67886e04fbd953e2f8dcbbeacd3b
ribacot <remontdiskov1979@gmail.com>
2023-07-10 20:03:36 +0300

    upd

--- src/js/shopping_list/shopping.js
+++ src/js/shopping_list/shopping.js
@@ -1,40 +1,33 @@
-import fetch from './serviceBook';
 import { marcupListBooks } from './marcupListBooks';
+import bookIsEmptyMob from '../../images/shopping_List/IMG_9606 1.png';
+
 const listBooksEl = document.querySelector('.js-list-books');
-
-// async function addLocalStoredge(data) {
-//   localStorage.setItem('shopingBooks', JSON.stringify(await data()));
-// }
-
-// addLocalStoredge(fetch);
 
 try {
   const books = JSON.parse(localStorage.getItem('savedBooks'));
-  if (!books||!books.length) {
- isEmpty()
+  if (!books || !books.length) {
+    isEmpty();
   } else {
     listBooksEl.innerHTML = marcupListBooks(books);
+
     listBooksEl.addEventListener('click', onRemuveCard);
 
     function onRemuveCard(e) {
-      console.log(e.target);
-
       if (!e.target.classList.contains('js-trash')) {
         return;
       }
-      console.log(e.target.dataset.id);
+      const shoppingCardId =
+        e.target.dataset.id ?? e.target.closest('.js-trash-id').dataset.id;
 
-      console.log('targetId', e.target.dataset.id);
-      const indexDel = books.findIndex(
-        ({ _id }) => _id === e.target.dataset.id
-      );
-      console.log(indexDel);
+      const indexDel = books.findIndex(({ _id }) => _id === shoppingCardId);
       if (indexDel !== -1) {
         books.splice(indexDel, 1);
 
         localStorage.setItem('savedBooks', JSON.stringify(books));
 
-        if(!books.length){return isEmpty()}
+        if (!books.length) {
+          return isEmpty();
+        }
         listBooksEl.innerHTML = marcupListBooks(books);
       }
     }
@@ -44,6 +37,8 @@ try {
 }
 
 function isEmpty() {
-     listBooksEl.innerHTML = `<p>This page is empty, add some books and proceed to order.</p><img src="./images/shopping_List/IMG_9606 1.png" alt="">
+  listBooksEl.innerHTML = `<div class="is-empty">
+  <p class="text-empty">This page is empty, add some books and proceed to order.</p>
+  <img src="${bookIsEmptyMob}" alt=""/></div>
 `;
 }
