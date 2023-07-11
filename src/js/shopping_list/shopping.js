83afc4160756895fff3f13749e5f9be6bbf9b286
ribacot <remontdiskov1979@gmail.com>
2023-07-11 17:29:49 +0300

    test

--- src/js/shopping_list/shopping.js
+++ src/js/shopping_list/shopping.js
@@ -1,50 +1,26 @@
-import fetch from './serviceBook';
-import bookIsEmptyMob from '../../images/shopping_List/IMG_9606 1.png';
+import fetch from './serviceBook'
 import { marcupListBooks } from './marcupListBooks';
+import { isActivePage } from '../is-active-page';
+import { isEmpty } from './isEmpty';
+import { onRemuveCard } from './remuve-card';
+
 const listBooksEl = document.querySelector('.js-list-books');
+const serchActivEl = document.querySelector('.home');
+const activPage = document.querySelector('.shopping-list');
+let books = null;
 
-// async function addLocalStoredge(data) {
-//   localStorage.setItem('shopingBooks', JSON.stringify(await data()));
-// }
-
-// addLocalStoredge(fetch);
+isActivePage.call(activPage);
 
 try {
-  const books = JSON.parse(localStorage.getItem('savedBooks'));
+  books = JSON.parse(localStorage.getItem('savedBooks'));
   if (!books || !books.length) {
     isEmpty();
   } else {
     listBooksEl.innerHTML = marcupListBooks(books);
-
     listBooksEl.addEventListener('click', onRemuveCard);
-
-    function onRemuveCard(e) {
-      if (!e.target.classList.contains('js-trash')) {
-        return;
-      }
-      const shoppingCardId =
-        e.target.dataset.id ?? e.target.closest('.js-trash-id').dataset.id;
-
-      const indexDel = books.findIndex(({ _id }) => _id === shoppingCardId);
-      if (indexDel !== -1) {
-        books.splice(indexDel, 1);
-
-        localStorage.setItem('savedBooks', JSON.stringify(books));
-
-        if (!books.length) {
-          return isEmpty();
-        }
-        listBooksEl.innerHTML = marcupListBooks(books);
-      }
-    }
   }
 } catch (err) {
   console.log(err);
 }
 
-function isEmpty() {
-  listBooksEl.innerHTML = `<div class="is-empty">
-  <p class="text-empty">This page is empty, add some books and proceed to order.</p>
-  <img src="${bookIsEmptyMob}" alt=""/></div>
-`;
-}
+export { listBooksEl, books };
