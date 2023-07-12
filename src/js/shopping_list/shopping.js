abd4fdb0007a6bc931b2dc506d8d3a2830d5cac9
ribacot <remontdiskov1979@gmail.com>
2023-07-12 13:50:23 +0300

    upd_css

--- src/js/shopping_list/shopping.js
+++ src/js/shopping_list/shopping.js
@@ -1,8 +1,8 @@
-import fetch from './serviceBook'
+import fetch from './serviceBook';
 import { marcupListBooks } from './marcupListBooks';
 import { isActivePage } from '../is-active-page';
 import { isEmpty } from './isEmpty';
-import { onRemuveCard } from './remuve-card';
+import { onRemoveCard } from './remuve-card';
 
 const listBooksEl = document.querySelector('.js-list-books');
 const serchActivEl = document.querySelector('.home');
@@ -17,7 +17,7 @@ try {
     isEmpty();
   } else {
     listBooksEl.innerHTML = marcupListBooks(books);
-    listBooksEl.addEventListener('click', onRemuveCard);
+    listBooksEl.addEventListener('click', onRemoveCard);
   }
 } catch (err) {
   console.log(err);
