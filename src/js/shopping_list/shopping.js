adfaf562f6ea74a4000e1dd658966a23ce26ad37
Tanyapavliuk <122897433+Tanyapavliuk@users.noreply.github.com>
2023-07-12 12:30:09 +0300

    Revert "Merge pull request #89 from Tanyapavliuk/Oleksii-home"
    This reverts commit 7b277fb480ca0df93f48eabff6830d2556c0c1ac, reversing
    changes made to 6086f1e943c087ed03539a28c83b388f948c217e.

--- src/js/shopping_list/shopping.js
+++ src/js/shopping_list/shopping.js
@@ -1,12 +1,13 @@
 import fetch from './serviceBook'
 import { marcupListBooks } from './marcupListBooks';
-export const listBooksEl = document.querySelector('.js-list-books');
-import { resetPag } from './pagination';
-import { isPagination } from './pagination';
-// async function addLocalStoredge(data) {
-//   localStorage.setItem('shopingBooks', JSON.stringify(await data()));
-// }
+import { isActivePage } from '../is-active-page';
+import { isEmpty } from './isEmpty';
+import { onRemuveCard } from './remuve-card';
 
+const listBooksEl = document.querySelector('.js-list-books');
+const serchActivEl = document.querySelector('.home');
+const activPage = document.querySelector('.shopping-list');
+let books = null;
 
 isActivePage.call(activPage);
 
@@ -16,30 +17,8 @@ try {
     isEmpty();
   } else {
     listBooksEl.innerHTML = marcupListBooks(books);
-    isPagination();
     listBooksEl.addEventListener('click', onRemuveCard);
-
-    function onRemuveCard(e) {
-      if (!e.target.classList.contains('js-trash')) {
-        return;
-      }
-      const shoppingCardId =
-        e.target.dataset.id ?? e.target.closest('.js-trash-id').dataset.id;
-
-      const indexDel = books.findIndex(({ _id }) => _id === shoppingCardId);
-      if (indexDel !== -1) {
-        books.splice(indexDel, 1);
-
-        localStorage.setItem('savedBooks', JSON.stringify(books));
-
-        if (!books.length) {
-          return isEmpty();
-        }
-        listBooksEl.innerHTML = marcupListBooks(books);
-        qAfterDel = JSON.parse(localStorage.getItem('savedBooks'));
-        resetPag(qAfterDel.length);
-      }
-    }
+  }
 } catch (err) {
   console.log(err);
 }
